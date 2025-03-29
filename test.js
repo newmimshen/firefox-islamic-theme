93a6e060a94ad4b9f47d0bf0d4628d58f51b873b
mom <mohammad.sharafi.1994.iranian@gmail.com>
2025-03-29 15:47:58 +0330

    add all required images for v1 height = 90 px

--- test.js
+++ test.js
@@ -1,10 +1,14 @@
 const sunTheme = {
     images: {
-        theme_frame: "img/imam-ali-theme.png"
+        additional_backgrounds: ["img/imam-ali-theme.png"]
     },
     colors: {
         frame: "#ffffff",
         tab_background_text: "#111",
+        frame_inactive: "green",
     },
-};
+    properties: {
+        additional_backgrounds_alignment:['left top'],
+    }
+    };
 browser.theme.update(sunTheme);
