e7805ed05d65bbf5a09226bf0ce493184d0d605e
mom <mohammad.sharafi.1994.iranian@gmail.com>
2025-03-30 07:22:03 +0330

    carving a masterpiece: test and debugging/ add bkground feautres like how to handle each persons for each day and their pngs

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const sunTheme = {
+let sunTheme = {
     images: {
         additional_backgrounds: ["img/imam-ali-theme.png"]
     },
@@ -11,4 +11,43 @@ const sunTheme = {
         additional_backgrounds_alignment:['left top'],
     }
     };
-browser.theme.update(sunTheme);
+//browser.theme.update(sunTheme); // it is a required api for firefox theme
+//sunTheme['images']['additional_backgrounds'] = 'img/selected/imam-ali';
+//console.log(sunTheme)
+const weekDays = ['yekshanbe', 'doshanbe', 'seshanbe','chaharshanbe', 'panjshanbe', 'jome', 'shanbe'];
+const sunPersons = {
+    nabi: ['/img/selected/payambar/1.png', '/img/selected/payambar/2.png'],
+    ali: ['/img/selected/imam-ali/1.png', '/img/selected/imam-ali/2.png', '/img/selected/imam-ali/3.png'],
+    hasan: ['/img/selected/imam-hasan/1.png', '/img/selected/imam-hasan/2.png'],
+    hussain: ['/img/selected/imam-hussain/1.png', '/img/selected/imam-hussain/2.png'],
+    sajjad: ['/img/selected/imam-sajjad/1.png','/img/selected/imam-sajjad/2.png', '/img/selected/imam-sajjad/3.png', '/img/selected/imam-sajjad/4.png'],
+    bagir: ['/img/selected/imam-bagher/1.png', '/img/selected/imam-bagher/2.png', '/img/selected/imam-bagher/3.png'],
+    sadigh: ['/img/selected/imam-sadegh/1.png', '/img/selected/imam-sadegh/2.png', '/img/selected/imam-sadegh/3.png', '/img/selected/imam-sadegh/4.png'],
+    kazim: ['/img/selected/imam-kazem/1.png', '/img/selected/imam-kazem/2.png', '/img/selected/imam-kazem/3.png'],
+    reza: ['/img/selected/imam-reza/1.png', '/img/selected/imam-reza/2.png', '/img/selected/imam-reza/3.png'],
+    javad: ['/img/selected/imam-javad/1.png', '/img/selected/imam-javad/2.png', '/img/selected/imam-javad/3.png', '/img/selected/imam-javad/4.png'],
+    hadi: ['/img/selected/imam-hadi/1.png', '/img/selected/imam-hadi/2.png', '/img/selected/imam-hadi/3.png', '/img/selected/imam-hadi/4.png', '/img/selected/imam-hadi/5.png'],
+    askari: ['/img/selected/imam-askari/1.png', '/img/selected/imam-askari/2.png', '/img/selected/imam-askari/3.png', '/img/selected/imam-askari/4.png'],
+    hojjat: ['/img/selected/imam-zaman/1.png', '/img/selected/imam-zaman/2.png']
+};
+const persons = ['nabi', 'ali', 'hasan', 'hussain', 'sajjad', 'bagir', 'sadigh', 'kazim', 'reza', 'javad', 'hadi', 'askari' ,'hojjat'];
+let time = new Date().getDay();
+//console.log(time + ": Today is " + weekDays[time]);
+//console.log(sunPersons.nabi[0]);
+//console.log(sunPersons.ali[Math.floor(Math.random() * 3)])
+//console.log(persons [Math.floor(Math.random() * 2)+2]);
+p = persons [Math.floor(Math.random() * 2)+2];
+console.log(sunPersons[p][Math.floor(Math.random() * 2)]);
+
+//console.log(sunPersons [sunPersons[Math.floor(Math.random() * 2)+2]][Math.floor(Math.random() * 3)]);
+function chooseSun () {
+    switch(time){
+        case 0:
+            sunTheme.images.additional_backgrounds = sunPersons.ali[Math.floor(Math.random() * 3)];
+            break;
+        case 1:
+            p = sunPersons[Math.floor(Math.random() * 2)+2];
+            sunTheme.images.additional_backgrounds = sunPersons[p][Math.floor(Math.random() * 2)];
+            break;
+    }
+}
