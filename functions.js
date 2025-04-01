9586f17cd7ca172a802101e788b85bca6b4624c7
mom <mohammad.sharafi.1994.iranian@gmail.com>
2025-04-01 13:14:55 +0330

    a version to work on local ide   couldnt find a way to work on firefox console with import/export, so i keep this version for updating and maintaining

---
+++ functions.js
@@ -0,0 +1,39 @@
+import { sunTheme, persons, time, sunPersons } from './variables.js';
+
+
+export function chooseLengthOfArray (nameOfSun){ //get length
+    return nameOfSun.length;
+}
+export function chooseRandomItem (array,length,minimum){ // get person/image
+    return array[Math.floor(Math.random() * length) + minimum];
+}
+
+export function chooseSun () {
+    let p;
+    switch(time){
+        case 0:
+            sunTheme.images.additional_backgrounds = [chooseRandomItem(sunPersons.ali,chooseLengthOfArray(sunPersons.ali),0)];
+            break;
+        case 1:
+            p = chooseRandomItem(persons,2,2);
+            sunTheme.images.additional_backgrounds = [sunPersons[p][Math.floor(Math.random() * chooseLengthOfArray(sunPersons[p]))]];
+            break;
+        case 2:
+            p = chooseRandomItem(persons,3,4);
+            sunTheme.images.additional_backgrounds = [sunPersons[p][Math.floor(Math.random() * chooseLengthOfArray(sunPersons[p]))]];
+            break;
+        case 3:
+            p = chooseRandomItem(persons,4,7);
+            sunTheme.images.additional_backgrounds = [sunPersons[p][Math.floor(Math.random() * chooseLengthOfArray(sunPersons[p]))]];
+            break;
+        case 4:
+            sunTheme.images.additional_backgrounds = [sunPersons.askari[Math.floor(Math.random() * chooseLengthOfArray(sunPersons.askari))]];
+            break;
+        case 5:
+            sunTheme.images.additional_backgrounds = [sunPersons.hojjat[Math.floor(Math.random() * chooseLengthOfArray(sunPersons.hojjat))]];
+            break;
+        case 6:
+            sunTheme.images.additional_backgrounds = [sunPersons.nabi[Math.floor(Math.random() * chooseLengthOfArray(sunPersons.nabi))]];
+            break;
+    }
+}
---
+++ sun-persons.js
@@ -0,0 +1,8 @@
+import { chooseSun } from './functions.js';
+import { sunTheme, persons, time, sunPersons } from './variables.js';
+
+
+chooseSun();
+// browser.theme.update(sunTheme);
+// console.log(browser.theme.getCurrent());
+console.log(sunTheme);
